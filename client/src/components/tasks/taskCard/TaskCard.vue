20ea54a2cede0f1576b2072700c64c16bef92bc1
lyes <lyes.aici95@yahoo.fr>
2018-05-01 21:03:15 +0200

    Code refactor

--- client/src/components/tasks/taskCard/TaskCard.vue
+++ client/src/components/tasks/taskCard/TaskCard.vue
@@ -42,7 +42,7 @@ export default {
       this.$store.dispatch('deleteTask', id);
     },
     editTask(task) {
-      this.$store.dispatch('setCurrentTask', task);
+      this.$store.commit('setCurrentTask', task);
     },
   },
 };
